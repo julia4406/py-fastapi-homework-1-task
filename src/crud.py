322c151f1a90f1c76e5886cce356b5eeb9c968e3
Julia Alexeeva <julia4406@gmail.com>
2025-03-12 21:12:33 +0200

    Solution

--- src/crud.py
+++ src/crud.py
@@ -18,13 +18,9 @@ async def get_all_movies_paginated(
         page: int,
         per_page: int
 ):
-    # з якого елемента починати вибірку (напр page = 2 і per_page = 10 -> з 10
     offset = (page - 1) * per_page
 
-    # Звертаємось до бази даних, повертає об'єкт, з потрібним зрізом (offset),
-    # limit - відобразить кількість на сторінці
     result = await db.execute(select(MovieModel))
-    # через scalars отримуємо результати (як objects)
     all_movies = result.scalars().all()
 
     movies = all_movies[offset:offset + per_page]
@@ -32,13 +28,12 @@ async def get_all_movies_paginated(
     total_pages = total_items // per_page + (
         1 if total_items % per_page != 0 else 0
     )
-    # previous page
+
     if page <= 1:
         prev_page = None
     else:
         prev_page = f"/theater/movies/?page={page - 1}&per_page={per_page}"
 
-    # next page
     if page >= total_pages:
         next_page = None
     else:
